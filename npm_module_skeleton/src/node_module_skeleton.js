59627a116f00305c9a2178905e5f08e5d58edb43
Justin Tanner <work@jwtanner.com>
2024-09-27 11:29:09 +0700

    Removed old projects and start a new node script

--- npm_module_skeleton/src/node_module_skeleton.js
+++
@@ -1,5 +0,0 @@
-const NodeModuleSkeleton = (input) => {
-  return "Hello from inside a module!";
-};
-
-export default NodeModuleSkeleton;
--- npm_module_skeleton/tests/index_prop_test.js
+++
@@ -1,8 +0,0 @@
-import { testProp, fc } from "ava-fast-check";
-import NodeModuleSkeleton from "../src/node_module_skeleton.js";
-
-testProp("outputs something", [fc.string()], (t) => {
-  var output = NodeModuleSkeleton();
-
-  t.true(typeof output === "string" || myVar instanceof String);
-});
